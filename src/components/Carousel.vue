15aa25258901627bebeb07059e5715d3c0f13299
MBaihaqi <baihaqi1401@student.ub.ac.id>
2024-09-15 18:29:50 +0700

    dari src

---
+++ src/components/Carousel.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="carousel">
+        <slot></slot>
+
+        <button @click="next" class="next">Next</button>
+        <button @click="prev" class="prev">Prev</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        next() {
+            this.$emit('next')
+        },
+        prev() {
+            this.$emit('prev')
+        }
+    }
+}
+</script>
+<style>
+  
+ 
+</style>
